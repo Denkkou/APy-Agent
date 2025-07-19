f0cc4af66355c2ef0a0344fd4c46779cb8a4eae7
Denkkou <JoeSchoDev@gmail.com>
2025-07-19 12:13:10 +0300

    Added verbose flag to display more context

--- main.py
+++ main.py
@@ -2,25 +2,40 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = ""
+    user_prompt = ""
 
-    if sys.argv[1] != None:
-        prompt = sys.argv[1]
+    # Prompt
+    if len(sys.argv) > 1:
+        user_prompt = sys.argv[1]
     else:
         print("Error, no prompt argument given")
         return 1
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
-        
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages
+        )
+ 
+    # Flags
+    if len(sys.argv) > 2:
+        match sys.argv[2]:
+            case "--verbose":
+                print(f"User prompt: {user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(f"\n{response.text}")
     
 
 if __name__ == "__main__":
